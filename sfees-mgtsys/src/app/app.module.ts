845bca11f8f98391b1a26084cb77b44c2e681d78
trucktar <57882685+trucktar@users.noreply.github.com>
2022-05-26 13:00:56 +0300

    Add ng-bootstrap library

--- sfees-mgtsys/src/app/app.module.ts
+++ sfees-mgtsys/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { SortPipe } from './sort.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SortPipe } from './sort.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

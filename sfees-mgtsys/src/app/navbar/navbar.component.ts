63d1ee23f1e117566c574d23c51f819baa97c9c8
trucktar <57882685+trucktar@users.noreply.github.com>
2022-05-26 15:07:37 +0300

    Make the navbar bootstrap compliant

--- sfees-mgtsys/src/app/navbar/navbar.component.ts
+++ sfees-mgtsys/src/app/navbar/navbar.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isMenuCollapsed = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

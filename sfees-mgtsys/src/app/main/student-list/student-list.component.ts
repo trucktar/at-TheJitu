61b4ca9b838ed214ab8ed2a63c8376ca1c952399
trucktar <57882685+trucktar@users.noreply.github.com>
2022-05-27 12:50:01 +0300

    Implement StudentService in StudentListComponent

--- sfees-mgtsys/src/app/main/student-list/student-list.component.ts
+++ sfees-mgtsys/src/app/main/student-list/student-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Student } from '../shared/student';
+import { StudentService } from '../shared/student.service';
+
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
-  styleUrls: ['./student-list.component.css']
+  styleUrls: ['./student-list.component.css'],
 })
 export class StudentListComponent implements OnInit {
-
-  constructor() { }
+  students: Student[] = [];
+  constructor(private _studentService: StudentService) {}
 
   ngOnInit(): void {
+    this.students = this._studentService.getAll();
   }
-
 }

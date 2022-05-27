992eb8f65f3f4606d28b6da100cd68c53435aeeb
trucktar <57882685+trucktar@users.noreply.github.com>
2022-05-27 12:54:47 +0300

    Reorder and fix component imports and declarations

--- sfees-mgtsys/src/app/app.module.ts
+++ sfees-mgtsys/src/app/app.module.ts
@@ -1,28 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from "./login/login.component";
 import { FooterComponent } from './footer/footer.component';
 import { StudentListComponent } from './main/student-list/student-list.component';
+import { StudentDetailComponent } from './main/student-detail/student-detail.component';
+// Pipes
 import { SortPipe } from './sort.pipe';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MainComponent,
+    LoginComponent,
     FooterComponent,
+    StudentListComponent,
+    StudentDetailComponent,
     SortPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

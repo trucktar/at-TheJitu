6b4ee4e73dafd399dc673a0f74b8abad001f046c
trucktar <57882685+trucktar@users.noreply.github.com>
2022-05-29 15:38:32 +0300

    Define and set up a reactive form and data model

--- sfees-mgtsys/src/app/main/student-add/student-add.component.ts
+++ sfees-mgtsys/src/app/main/student-add/student-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Student } from '../shared/student';
+import { StudentService } from '../shared/student.service';
 
 @Component({
   selector: 'app-student-add',
@@ -6,7 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-add.component.css'],
 })
 export class StudentAddComponent implements OnInit {
-  constructor() {}
+  studentForm = new FormGroup({
+    regId: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    contactDetails: new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(/@thejitu\.com$/i),
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        Validators.pattern(/^01|7/),
+      ]),
+    }),
+    course: new FormControl('', Validators.required),
+    balance: new FormControl(0),
+  });
+
+  constructor(
+    private _router: Router,
+    private _studentService: StudentService
+  ) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    this._studentService.addOne(<Student>this.studentForm.value);
+    this._router.navigate(['/']);
+  }
 }
